130da1e8119bdf1932f948c728b6e10720c035f9
SamLevSa <samueldesahleitao@gmail.com>
2026-05-31 23:55:58 -0300

    implementacao da feature de READ

--- doacoes/views.py
+++ doacoes/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import Doacao
+
+def listar_doacoes(request):
+    doacoes = Doacao.objects.all()
+    resultado = []
+    
+    for doacao in doacoes:
+        resultado.append({
+            "id": doacao.id,
+            "nome_alimento": doacao.nome_alimento,
+            "quantidade": doacao.quantidade,
+            "data_validade": str(doacao.data_validade),
+            "data_entrada": str(doacao.data_entrada),
+        })
+    
+    return JsonResponse(resultado, safe=False)
--- ecoservice/urls.py
+++ ecoservice/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from doacoes.views import listar_doacoes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('doacoes/', listar_doacoes),
 ]
